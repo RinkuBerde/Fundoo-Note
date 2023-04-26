607b78c4dce8486779c6be7645b4be076d8abbb5
rinkuberde <rinkuberde@gmail.com>
2023-04-26 23:17:08 +0530

    desig forgot-password and reset-password page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { LoginComponent } from './components/login/login.component';
+import { ResetpasswordComponent } from './components/resetpassword/resetpassword.component';
+import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    LoginComponent,
+    ResetpasswordComponent,
+    ForgotPasswordComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,MatFormFieldModule,MatInputModule,
+    
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
